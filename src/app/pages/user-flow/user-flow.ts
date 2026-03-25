6c14457edfb0751eeb113aace2a41115c19a7c3d
aki-xentra <renz.magsakay.xentra@outlook.com>
2026-03-26 01:16:29 +0800

    added Main content pages

---
+++ src/app/pages/user-flow/user-flow.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-flow',
+  imports: [],
+  templateUrl: './user-flow.html',
+  styleUrl: './user-flow.scss',
+})
+export class UserFlow {}
