35d9feef13215814a2a42b2f836535995f776ce1
aki-xentra <renz.magsakay.xentra@outlook.com>
2026-04-08 00:25:42 +0800

    added dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, AfterViewInit } from '@angular/core';
+import { DatePickerComponent } from '../../components/date-picker.component/date-picker.component';
+import Chart from 'chart.js/auto'
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterLink],
+  imports: [ DatePickerComponent ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {}
+export class Dashboard implements AfterViewInit {
+  isCustom = false;
+
+  onPresetChange(event: any) {
+    this.isCustom = event.target.value === 'custom';
+  }
+
+  ngAfterViewInit(): void {
+    this.createChart();
+  }
+  createChart() {
+    const canvas = document.getElementById('salesChart') as HTMLCanvasElement;
+
+    if (!canvas) return;
+
+    new Chart(canvas, {
+      type: 'line',
+      data: {
+        labels: ['Mon', 'Tue', 'Wed', 'Thurs', 'Fri'],
+        datasets: [
+          {
+            data: [40, 52, 72, 60, 67],
+            borderColor: '#1aa36f',
+            backgroundColor: 'rgba(26,163,111,0.2)',
+            fill: true,
+            tension: 0.4,
+            pointRadius: 3
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: false
+          }
+        },
+        scales: {
+          x: {
+            grid: {
+              display: false
+            }
+          },
+          y: {
+            beginAtZero: true,
+            ticks: {
+              stepSize: 20
+            }
+          }
+        }
+      }
+    });
+  }
+}
